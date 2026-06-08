344ba3c09f25c0a5e03e5e1051a3ab3263921a14
mehak-garg <mehakgarg741@gmail.com>
2026-06-08 14:53:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,17 +7,8 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
-const output = document.getElementById("output");
+
 const loading = document.getElementById("loading");
-const btn = document.getElementById("download-images-button");
-const error = document.getElementById("error");
-
-const images = [
-  { url: "https://picsum.photos/id/237/200/300" },
-  { url: "https://picsum.photos/id/238/200/300" },
-  { url: "https://picsum.photos/id/239/200/300" },
-];
-
 btn.addEventListener("click", () => {
   loading.innerText = "Loading...";
   error.innerText = "";
